0d6e5cdc942ed9a33949599ea811f34457d9302d
Luis Felipe Sánchez <luis.sanchezgu@usach.cl>
2013-08-26 07:54:56 -0400

    adios tío edgi

--- app/Model/Empleado.php
+++ app/Model/Empleado.php
@@ -30,7 +30,7 @@ class Empleado extends AppModel {
 			'message' => 'verifique el RUT'
 			)
 		)
-	);
+	;
 
 	//The Associations below have been created with all possible keys, those that are not needed can be removed
 
